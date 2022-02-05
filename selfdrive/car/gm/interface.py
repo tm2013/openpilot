be19a8d3549e00984a4e730a755ae096aca68fa2
darknight111 <75706333+darknight111@users.noreply.github.com>
2022-02-04 18:55:19 -0800

    Lateral and long changes

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -172,8 +172,8 @@ class CarInterface(CarInterfaceBase):
       ret.steerRateCost = 0.5
       ret.steerActuatorDelay = 0.
       ret.lateralTuning.pid.kpBP, ret.lateralTuning.pid.kiBP = [[10., 41.0], [10., 41.0]]
-      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.26], [0.01, 0.02]]
-      ret.lateralTuning.pid.kf = 0.0001
+      ret.lateralTuning.pid.kpV, ret.lateralTuning.pid.kiV = [[0.18, 0.26], [0.01, 0.021]]
+      ret.lateralTuning.pid.kf = 0.0002
       #steering parameters from Geniuth
       ret.steerMaxBP = [10., 25.]
       ret.steerMaxV = [1., 1.15]
@@ -239,9 +239,9 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor and candidate == CAR.BOLT_NR:
       # Assumes the Bolt is using L-Mode for regen braking.
       ret.longitudinalTuning.kpBP = [0., 35]
-      ret.longitudinalTuning.kpV = [0.25, 0.45] 
+      ret.longitudinalTuning.kpV = [0.25, 0.49] 
       ret.longitudinalTuning.kiBP = [0., 35.] 
-      ret.longitudinalTuning.kiV = [0.22, 0.29]
+      ret.longitudinalTuning.kiV = [0.22, 0.34]
       ret.stoppingDecelRate = 0.2  # reach stopping target smoothly, brake_travel/s while trying to stop
       ret.stopAccel = 0. # Required acceleraton to keep vehicle stationary
       ret.vEgoStopping = 0.5  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
