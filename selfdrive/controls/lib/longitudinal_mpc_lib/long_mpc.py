e87a843f92a5db05777064607ba629e006b7959e
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-04-06 13:29:10 -0700

    Change stop distance to default

--- selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
+++ selfdrive/controls/lib/longitudinal_mpc_lib/long_mpc.py
@@ -51,7 +51,7 @@ T_DIFFS = np.diff(T_IDXS, prepend=[0.])
 MIN_ACCEL = -3.5
 T_FOLLOW = 1.35
 COMFORT_BRAKE = 3.0
-STOP_DISTANCE = 7.0
+STOP_DISTANCE = 6.0
 
 def get_stopped_equivalence_factor(v_lead):
   return (v_lead**2) / (2 * COMFORT_BRAKE)
