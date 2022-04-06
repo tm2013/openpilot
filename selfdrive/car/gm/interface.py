21a02a526f8541da6b79ae8bb94c10d10914260d
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-04-05 21:23:31 -0700

    Testing stopping condition. CAUTION

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -242,9 +242,9 @@ class CarInterface(CarInterfaceBase):
       ret.longitudinalTuning.kiBP = [0., 35.] 
       ret.longitudinalTuning.kiV = [0.22, 0.34]
       ret.stoppingDecelRate = 0.17  # reach stopping target smoothly, brake_travel/s while trying to stop
-      ret.stopAccel = 20.0 # Required acceleraton to keep vehicle stationary
-      ret.vEgoStopping = 0.5  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
-      ret.vEgoStarting = 0.5  # Speed at which the car goes into starting state, when car starts requesting starting accel,
+      ret.stopAccel = 50.0 # Required acceleraton to keep vehicle stationary
+      ret.vEgoStopping = 0.65  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
+      ret.vEgoStarting = 0.65  # Speed at which the car goes into starting state, when car starts requesting starting accel,
       # vEgoStarting needs to be > or == vEgoStopping to avoid state transition oscillation
       ret.stoppingControl = True
       ret.longitudinalTuning.deadzoneBP = [0.]
