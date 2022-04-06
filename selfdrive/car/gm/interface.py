2ec3c3a834823ecb12959ecb0a2b692fe317945e
darknight11ish <102553777+darknight11ish@users.noreply.github.com>
2022-04-06 13:31:39 -0700

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -238,13 +238,13 @@ class CarInterface(CarInterfaceBase):
     if ret.enableGasInterceptor and candidate == CAR.BOLT_NR:
       # Assumes the Bolt is using L-Mode for regen braking.
       ret.longitudinalTuning.kpBP = [0., 35.]
-      ret.longitudinalTuning.kpV = [0.18, 0.41] 
+      ret.longitudinalTuning.kpV = [0.18, 0.4] 
       ret.longitudinalTuning.kiBP = [0., 35.] 
       ret.longitudinalTuning.kiV = [0.22, 0.34]
       ret.stoppingDecelRate = 0.17  # reach stopping target smoothly, brake_travel/s while trying to stop
-      ret.stopAccel = 50.0 # Required acceleraton to keep vehicle stationary
-      ret.vEgoStopping = 0.65  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
-      ret.vEgoStarting = 0.65  # Speed at which the car goes into starting state, when car starts requesting starting accel,
+      ret.stopAccel = 5.0 # Required acceleraton to keep vehicle stationary
+      ret.vEgoStopping = 0.5  # Speed at which the car goes into stopping state, when car starts requesting stopping accel
+      ret.vEgoStarting = 0.5  # Speed at which the car goes into starting state, when car starts requesting starting accel,
       # vEgoStarting needs to be > or == vEgoStopping to avoid state transition oscillation
       ret.stoppingControl = True
       ret.longitudinalTuning.deadzoneBP = [0.]
